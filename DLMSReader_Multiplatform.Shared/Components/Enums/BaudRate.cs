8f96470f14fed364f1a6aa9c5afeb3675f6f0051
Martinececicek <martin.zarecky@email.cz>
2025-04-28 11:42:30 +0200

    Updated AddDevicePage

---
+++ DLMSReader_Multiplatform.Shared/Components/Enums/BaudRate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLMSReader_Multiplatform.Shared.Components.Enums
+{
+    public enum BaudRate
+    {
+        B300 = 300,
+        B600 = 600,
+        B1200 = 1200,
+        B2400 = 2400,
+        B4800 = 4800,
+        B9600 = 9600,
+        B19200 = 19200,
+        B38400 = 38400,
+        B57600 = 57600,
+        B115200 = 115200
+    }
+}
